0be3b11b94b68d840a8b71fb9065973a975f1d71
chrislzm <me@chrisleung.com>
2018-01-16 18:13:45 -0800

    fix: Now using nextLine to catch empty strings

--- src/com/chrisleung/leetcode/tests/Test_2StringInputs_IntOutput.java
+++ src/com/chrisleung/leetcode/tests/Test_2StringInputs_IntOutput.java
@@ -23,7 +23,7 @@ import com.chrisleung.leetcode.solutions.*;
 public class Test_2StringInputs_IntOutput {
 	
 	/* NOTE: Modify path and filename here as needed */
-	static private final String TEST_DATA_RELATIVE_PATH = "data/Problem_712_Minimum_ASCII_Delete_Sum.txt";
+	static private final String TEST_DATA_RELATIVE_PATH = "data/Problem_072_Edit_Distance.txt";
 	
     static private int testCaseNum = 0;
     private static Scanner in = null;
@@ -40,9 +40,9 @@ public class Test_2StringInputs_IntOutput {
     	while(in.hasNext()) {
     		/* NOTE: Modify file input format as needed */
 			// Read test case and solution
-			String inputString1 = in.next();
-			String inputString2 = in.next();
-			String expectedString = in.next();
+			String inputString1 = in.nextLine();
+			String inputString2 = in.nextLine();
+			String expectedString = in.nextLine();
 			Integer expected = Integer.valueOf(expectedString);
 			Object[] testCase = new Object[3];
 			testCase[0] = inputString1;
@@ -64,8 +64,8 @@ public class Test_2StringInputs_IntOutput {
 	@Test
 	public void test() {
 		/* NOTE: Modify the two following lines as needed */
-		Problem_712_Minimum_ASCII_Delete_Sum solution = new Problem_712_Minimum_ASCII_Delete_Sum();
-		Integer result  = solution.minimumDeleteSum(fInput1, fInput2);
+		Problem_072_Edit_Distance_v3 solution = new Problem_072_Edit_Distance_v3();
+		Integer result  = solution.minDistance(fInput1, fInput2);
 		
 		boolean passed = fExpected.equals(result);
 		System.out.println(String.format("Test Case #%s  Passed: %s  Inputs: %s %s  Output Expected: %s  Output Actual: %s ",testCaseNum,passed,fInput1,fInput2,fExpected,result));
