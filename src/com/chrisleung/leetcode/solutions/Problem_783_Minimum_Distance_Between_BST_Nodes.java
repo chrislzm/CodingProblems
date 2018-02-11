ffe3bc00f0da52ca4763364c3dba64b4dd138a56
chrislzm <me@chrisleung.com>
2018-02-10 20:05:37 -0800

    Initial commit (Problem 783)

---
+++ src/com/chrisleung/leetcode/solutions/Problem_783_Minimum_Distance_Between_BST_Nodes.java
@@ -0,0 +1,31 @@
+package com.chrisleung.leetcode.solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem_783_Minimum_Distance_Between_BST_Nodes {
+    public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode(int x) { val = x; }
+    }
+    public int minDiffInBST(TreeNode root) {
+        List<Integer> l = new ArrayList<>();
+        dfs(root,l);
+        int prev = l.get(0);
+        int minDiff = Integer.MAX_VALUE;
+        for(int i=1; i<l.size(); i++) {
+            minDiff = Math.min(minDiff, l.get(i)-prev);
+            prev = l.get(i);
+        }
+        return minDiff;
+    }
+    
+    private void dfs(TreeNode root, List<Integer> l) {
+        if(root == null) return;
+        dfs(root.left,l);
+        l.add(root.val);
+        dfs(root.right,l);
+    }
+}
