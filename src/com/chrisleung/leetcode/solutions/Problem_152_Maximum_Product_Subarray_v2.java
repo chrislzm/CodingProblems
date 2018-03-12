53c98f7f4bd5a591d2abb7b5b038ea7e88e396a5
chrislzm <me@chrisleung.com>
2018-03-11 21:50:12 -0700

    refactor: Updated variable names for readability

--- src/com/chrisleung/leetcode/solutions/Problem_152_Maximum_Product_Subarray_v2.java
+++ src/com/chrisleung/leetcode/solutions/Problem_152_Maximum_Product_Subarray_v2.java
@@ -2,18 +2,18 @@ package com.chrisleung.leetcode.solutions;
 
 public class Problem_152_Maximum_Product_Subarray_v2 {
     public int maxProduct(int[] nums) {
-        int minToThisPoint = nums[0];
-        int maxToThisPoint = nums[0];
+        int currentMin = nums[0];
+        int currentMax = nums[0];
         int overallMax = nums[0];
         for(int i=1; i< nums.length; i++) {
-            if(nums[i] < 0) {
-                int tmp = maxToThisPoint;
-                maxToThisPoint = minToThisPoint;
-                minToThisPoint = tmp;
+            if(nums[i] < 0) { // Swap values
+                int tmp = currentMax;
+                currentMax = currentMin;
+                currentMin = tmp;
             }
-            maxToThisPoint = Math.max(nums[i],maxToThisPoint*nums[i]);
-            minToThisPoint = Math.min(nums[i],minToThisPoint*nums[i]);
-            overallMax = Math.max(overallMax, maxToThisPoint);
+            currentMax = Math.max(nums[i],currentMax*nums[i]);
+            currentMin = Math.min(nums[i],currentMin*nums[i]);
+            overallMax = Math.max(overallMax, currentMax);
         }
         return overallMax;
     }
