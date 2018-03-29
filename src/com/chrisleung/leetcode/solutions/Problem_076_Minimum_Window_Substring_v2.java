8f8e359dc3185df7f9636ddcc82d281f0a1fb400
chrislzm <me@chrisleung.com>
2018-03-29 10:05:58 -0700

    refactor: Updated variable names and comments

--- src/com/chrisleung/leetcode/solutions/Problem_076_Minimum_Window_Substring_v2.java
+++ src/com/chrisleung/leetcode/solutions/Problem_076_Minimum_Window_Substring_v2.java
@@ -1,37 +1,57 @@
 package com.chrisleung.leetcode.solutions;
 
 public class Problem_076_Minimum_Window_Substring_v2 {
+
     public String minWindow(String s, String t) {
-        int[] charFreq = new int[128];
-        for(char c : t.toCharArray()) {
-            charFreq[c]++;
+        if(s == null || t == null || s.length() == 0 || t.length() == 0) return "";
+        
+        char[] input = s.toCharArray();
+
+        // elementBalance[i]  =   Minimum frequency of element i that's required for a "valid" window
+        // elementBalance[i] < 0  We saw element i less times than required in the current window
+        // elementBalance[i] == 0 We saw element i exactly as many times as required in the current window
+        // elementBalance[i] > 0  We saw element i more times than required in the current window
+        // Important: The only way charBalance[i] == 0 is if it never occurs in string t or s OR
+        // we have found it exactly the required number of times. Otherwise it will always be negative.
+        
+        int[] elementBalance = new int[128];
+         
+        // Update balances of required elements
+        for(char requiredElement : t.toCharArray()) {
+            elementBalance[requiredElement]--;
         }
-        int balance = t.length();
-        int windowStartIndex = 0;
-        int windowEndIndex = 0;
-        int minWindowLen = Integer.MAX_VALUE;
+                
+        // # of required elements not found in the current window
+        int overallBalance = -t.length();
+        
+        // Tracks the min window found so far
         int minWindowStartIndex = 0;
-        char[] str = s.toCharArray();
-        while(windowEndIndex < str.length) {
-            char c = str[windowEndIndex];
-            if(charFreq[c] > 0)
-                balance--;
-            charFreq[c]--;
-            while(balance == 0) {
-                int curWindowLength = windowEndIndex+1-windowStartIndex;
-                if(curWindowLength < minWindowLen) {
-                    minWindowLen = curWindowLength;
-                    minWindowStartIndex = windowStartIndex;
-                }
-                char firstWindowChar = str[windowStartIndex];
-                if(charFreq[firstWindowChar] == 0) {
-                    balance++;
-                }
-                charFreq[firstWindowChar]++;
-                windowStartIndex++;
+        int minWindowLength = Integer.MAX_VALUE;
+        
+        int curWindowStartIndex = 0;
+        for(int curWindowEndIndex=0; curWindowEndIndex < input.length; curWindowEndIndex++) {
+            char curElement = input[curWindowEndIndex];
+            if(elementBalance[curElement] < 0) { // If this is a required element
+                overallBalance++; // We have found a required element
             }
-            windowEndIndex++;
+            elementBalance[curElement]++; 
+            // While all required elements are in the current window
+            while(overallBalance == 0) { 
+                // Updated minimum window found if needed
+                int curWindowLength = curWindowEndIndex-curWindowStartIndex+1;
+                if(curWindowLength < minWindowLength) {
+                    minWindowLength = curWindowLength;
+                    minWindowStartIndex = curWindowStartIndex;
+                }
+                // Shrink (minimize) this window size until we lose a required element
+                char elementToRemove = input[curWindowStartIndex];
+                curWindowStartIndex++;
+                if(elementBalance[elementToRemove] == 0) {
+                    overallBalance--; // We're losing the minimum required# of this element
+                }
+                elementBalance[elementToRemove]--;
+            }
         }
-        return minWindowLen == Integer.MAX_VALUE ? "" : s.substring(minWindowStartIndex,minWindowStartIndex+minWindowLen);
+        return minWindowLength == Integer.MAX_VALUE ? "" : s.substring(minWindowStartIndex,minWindowStartIndex+minWindowLength);
     }
 }
