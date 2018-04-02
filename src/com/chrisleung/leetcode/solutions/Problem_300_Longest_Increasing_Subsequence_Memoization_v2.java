6e508bff7d738523d88df88208d421c558c94dc2
chrislzm <me@chrisleung.com>
2018-04-01 23:58:44 -0700

    style: fixed indentation

--- src/com/chrisleung/leetcode/solutions/Problem_300_Longest_Increasing_Subsequence_Memoization_v2.java
+++ src/com/chrisleung/leetcode/solutions/Problem_300_Longest_Increasing_Subsequence_Memoization_v2.java
@@ -6,18 +6,18 @@ public class Problem_300_Longest_Increasing_Subsequence_Memoization_v2 {
         int numElements = nums.length;
         Integer[][] memo = new Integer[numElements+1][numElements+1];
         return lengthOfLISHelper(0,1, nums, numElements,memo);
-      }
+    }
 
     int lengthOfLISHelper(int prev, int cur, int[] nums, int numElements, Integer[][] memo) {
         if(cur > numElements) return 0;
         if(memo[prev][cur] == null) {
-          int longestExclude = lengthOfLISHelper(prev,cur+1,nums,numElements,memo);
-          int longestInclude = 0;
-          if(prev == 0 || nums[cur-1] > nums[prev-1]) {
-            longestInclude = 1 + lengthOfLISHelper(cur,cur+1,nums,numElements,memo);
-          }
-          memo[prev][cur] = Math.max(longestExclude,longestInclude);
+            int longestExclude = lengthOfLISHelper(prev,cur+1,nums,numElements,memo);
+            int longestInclude = 0;
+            if(prev == 0 || nums[cur-1] > nums[prev-1]) {
+                longestInclude = 1 + lengthOfLISHelper(cur,cur+1,nums,numElements,memo);
+            }
+            memo[prev][cur] = Math.max(longestExclude,longestInclude);
         }
         return memo[prev][cur];
-      }
+    }
 }
