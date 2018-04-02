73770b09621b8b95e4e990527a43a1bb909581b5
chrislzm <me@chrisleung.com>
2018-04-01 23:59:07 -0700

    style: fixed indentation

--- src/com/chrisleung/leetcode/solutions/Problem_300_Longest_Increasing_Subsequence_Tabulation_v2.java
+++ src/com/chrisleung/leetcode/solutions/Problem_300_Longest_Increasing_Subsequence_Tabulation_v2.java
@@ -5,17 +5,17 @@ public class Problem_300_Longest_Increasing_Subsequence_Tabulation_v2 {
         int numElements = nums.length;
         int[][] table = new int[numElements+1][2];
         for(int cur=numElements; cur > 0; cur--) {
-          table[cur][0] = table[cur][1];
-          for(int prev=cur-1; prev >= 0; prev--) {
-            table[prev][0] = table[prev][1]; // Copy last row 
-            int longestExclude = table[prev][0];
-            int longestInclude = 0;
-            if(prev == 0 || nums[cur-1] > nums[prev-1]) {
-              longestInclude = 1 + table[cur][0];
+            table[cur][0] = table[cur][1];
+            for(int prev=cur-1; prev >= 0; prev--) {
+                table[prev][0] = table[prev][1]; // Copy last row 
+                int longestExclude = table[prev][0];
+                int longestInclude = 0;
+                if(prev == 0 || nums[cur-1] > nums[prev-1]) {
+                    longestInclude = 1 + table[cur][0];
+                }
+                table[prev][1] = Math.max(longestExclude,longestInclude);
             }
-            table[prev][1] = Math.max(longestExclude,longestInclude);
-          }
         }  
         return table[0][1];
-      }
+    }
 }
