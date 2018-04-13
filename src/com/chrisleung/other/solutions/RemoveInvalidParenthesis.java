ba710a37ed9e437cda4ee95f0317397417aee365
chrislzm <me@chrisleung.com>
2018-04-12 20:21:21 -0700

    style: Compact version

--- src/com/chrisleung/other/solutions/RemoveInvalidParenthesis.java
+++ src/com/chrisleung/other/solutions/RemoveInvalidParenthesis.java
@@ -9,8 +9,10 @@ import java.util.LinkedList;
  *
  */
 public class RemoveInvalidParenthesis {
+    
     // Removes unclosed parenthesis, and closed parenthesis without any opening parenthesis
     // e.g. "(()))" returns "(())". There may be other characters in the string
+    
     static String removeInvalidParenthesis(String s) {
         if (s == null || s.isEmpty()) return "";
         StringBuilder sb = new StringBuilder();
@@ -18,17 +20,10 @@ public class RemoveInvalidParenthesis {
         for(int i=0; i<s.length(); i++) {
             char c = s.charAt(i);
             if(c == ')') {
-                if(stack.isEmpty()) {
-                    c = ' ';
-                } else {
-                    stack.pop();
-                }
+                if(stack.isEmpty()) c = ' ';
+                else stack.pop();
             }
-            else {
-                if(c == '(') {
-                    stack.push(i);
-                }
-            }
+            else if(c == '(') stack.push(i);
             sb.append(c);
         }
         for(int i : stack) {
