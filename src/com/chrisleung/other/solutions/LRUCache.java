7400590222d9af234237f7c3cc4ecb12377258cd
chrislzm <me@chrisleung.com>
2018-04-12 17:57:51 -0700

    feat: Updated code and test cases

--- src/com/chrisleung/other/solutions/LRUCache.java
+++ src/com/chrisleung/other/solutions/LRUCache.java
@@ -1,5 +1,6 @@
 package com.chrisleung.other.solutions;
 
+import java.util.HashMap;
 import java.util.Map;
 
 // Return the object if it's in the cache, otherwise null
@@ -7,20 +8,22 @@ import java.util.Map;
 
 public class LRUCache<K,V> {
     
-    private class Node<E> {
-        Node<E> next;
-        Node<E> prev;
-        E value;
-        Node(E d) {
-            value = d;
+    private class Node<T1,T2> {
+        Node<T1,T2> next;
+        Node<T1,T2> prev;
+        T1 key;
+        T2 value;
+        Node(T1 k, T2 v) {
+            key = k;
+            value = v;
         }
     }
     
     // Map from data to Node
-    private Map<K,Node<V>> map;
+    private Map<K,Node<K,V>> map = new HashMap<>();
     
     // header.prev = bottom of cache, header.next = top of cache
-    private Node<V> header;
+    private Node<K,V> header;
     
     private int capacity;
     private int size;
@@ -30,49 +33,73 @@ public class LRUCache<K,V> {
             throw new Exception("Invalid cache size.");
         }
         capacity = c;
-        header = new Node<V>(null);
+        header = new Node<K,V>(null,null);
         header.next = header;
         header.prev = header;
     }
     
     private boolean isFull() {
-        return map.size() == capacity;
+        return size() == capacity;
     }
     
-    private Node<V> getNode(K key) {
+    private Node<K,V> getNode(K key) {
         return map.get(key);
     }
 
     public void add(K k, V v) {
-        Node<V> node = getNode(k);
+        Node<K,V> node = getNode(k);
         if(node == null) {
             // Create new node
-            node = new Node<V>(v);
+            node = new Node<K,V>(k,v);
+            map.put(k,node);
         } else {
             // Remove the node from wherever it is
             remove(node);
         }
         if(isFull()) {
+            // Remove last item in cache
             remove(header.prev);
         }
+        addBefore(node,header.next);
     }
     
-    private void addBefore(Node<V> node, Node<V> nextNode) {
+    private void addBefore(Node<K,V> node, Node<K,V> nextNode) {
         node.prev = nextNode.prev;
         node.next = nextNode;
         nextNode.prev = node;
         node.prev.next = node;
     }
     
-    private void remove(Node<V> node) {
+    private void remove(Node<K,V> node) {
         node.next.prev = node.prev;
         node.prev.next = node.next;
-        size--;
+        map.remove(node.key);
     }
     
     public int size() {
-        return size;
+        return map.size();
     }
-    // get(
+
+    public V get(K key) {
+        Node<K,V> n = map.get(key);
+        if(n == null) {
+            return null;
+        }
+        // Move node to front of cache
+        remove(n);
+        addBefore(n,header.next);
+        return n.value;
+    }
     
+    public static void main(String[] args) throws Exception {
+        LRUCache<Integer,Integer> cache = new LRUCache<>(3);
+        cache.add(1, 100);
+        cache.add(2, 200);
+        cache.add(3, 300);
+        cache.add(4, 400);
+        System.out.println(cache.get(1));
+        System.out.println(cache.get(2));
+        System.out.println(cache.get(3));
+        System.out.println(cache.get(4));
+    }
 }
