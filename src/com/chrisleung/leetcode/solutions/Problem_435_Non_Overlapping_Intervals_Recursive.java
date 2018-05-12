bf10feabf02e8d8202d7e52093e7f77b88cbe303
chrislzm <me@chrisleung.com>
2018-05-12 02:41:09 -0700

    doc: Added comments

--- src/com/chrisleung/leetcode/solutions/Problem_435_Non_Overlapping_Intervals_Recursive.java
+++ src/com/chrisleung/leetcode/solutions/Problem_435_Non_Overlapping_Intervals_Recursive.java
@@ -12,15 +12,17 @@ public class Problem_435_Non_Overlapping_Intervals_Recursive {
     private int getMinRemove(int i, Interval[] intervals, Integer[] memo) {
         if(i >= intervals.length-1) return 0;
         if(memo[i] == null) {
-            if(intervals[i].end <= intervals[i+1].start) {
+            if(intervals[i].end <= intervals[i+1].start) { // No overlap with the next interval
                 memo[i] = getMinRemove(i+1,intervals,memo);
-            } else {
+            } else { // Overlaps with the next interval
+                // 1. Calculate min if we keep this interval and skip all overlapping ones
                 int skipToIndex = i+2;
                 while(skipToIndex < intervals.length && intervals[i].end > intervals[skipToIndex].start) {
                     skipToIndex++; // While overlapping, remove interval
                 }
                 int numRemoved = skipToIndex - i - 1;
                 int removedIfKeep = numRemoved + getMinRemove(skipToIndex,intervals,memo);
+                // 2. Calculate min if we discard this interval
                 int removedIfSkip = 1 + getMinRemove(i+1,intervals,memo);
                 memo[i] = Math.min(removedIfKeep,removedIfSkip);
             }
