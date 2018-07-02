dbc03ab55165ba3c1229e21b1e362ad6866ea7c9
chrislzm <me@chrisleung.com>
2018-07-01 20:59:11 -0700

    refactor: Cleaned up code, made it faster

--- src/com/chrisleung/leetcode/solutions/Problem_047_Permutations_II.java
+++ src/com/chrisleung/leetcode/solutions/Problem_047_Permutations_II.java
@@ -1,6 +1,7 @@
 package com.chrisleung.leetcode.solutions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,22 +14,20 @@ public class Problem_047_Permutations_II {
         for(int n : nums) {
             map.put(n,map.getOrDefault(n,0)+1);
         }
-        List<Integer> permutation = new ArrayList<Integer>();
-        permuteUniqueHelper(map, nums.length, permutation, results);
+        permuteUniqueHelper(map, nums.length, new Integer[nums.length], 0, results);
         return results;
     }
-
-    private void permuteUniqueHelper(Map<Integer,Integer> m, int l, List<Integer> p, List<List<Integer>> r) {
-        if(l == 0) {
-            r.add(new ArrayList<>(p));
+    
+    private void permuteUniqueHelper(Map<Integer,Integer> m, int l, Integer[] p, int i, List<List<Integer>> r) {
+        if(i == l) {
+            r.add(Arrays.asList(Arrays.copyOf(p,l)));
             return;
         }
         for(int key : m.keySet()) {
             if(m.get(key) > 0) {
                 m.put(key,m.get(key)-1);
-                p.add(key);
-                permuteUniqueHelper(m,l-1,p,r);
-                p.remove(p.size()-1);
+                p[i] = key;
+                permuteUniqueHelper(m,l,p,i+1,r);
                 m.put(key,m.get(key)+1);
             }
         }
