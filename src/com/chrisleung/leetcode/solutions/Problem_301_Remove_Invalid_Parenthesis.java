d2edb2fba7a2a88e95bf17d91b08d797b2b7f389
chrislzm <me@chrisleung.com>
2018-07-20 00:31:30 -0700

    Initial commit: Problem 301 Remove Invalid Parenthesis

---
+++ src/com/chrisleung/leetcode/solutions/Problem_301_Remove_Invalid_Parenthesis.java
@@ -0,0 +1,34 @@
+package com.chrisleung.leetcode.solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem_301_Remove_Invalid_Parenthesis {
+    
+    public List<String> removeInvalidParentheses(String s) {
+        List<String> output = new ArrayList<>();
+        removeHelper(s, output, 0, 0, '(', ')');
+        return output;
+    }
+
+    public void removeHelper(String s, List<String> output, int iStart, int jStart, char openParen, char closedParen) {
+        int numOpenParen = 0, numClosedParen = 0;
+        for (int i = iStart; i < s.length(); i++) {
+            if (s.charAt(i) == openParen) numOpenParen++;
+            if (s.charAt(i) == closedParen) numClosedParen++;
+            if (numClosedParen > numOpenParen) { // We have an extra closed paren we need to remove
+                for (int j = jStart; j <= i; j++) // Try removing one at each position, skipping duplicates
+                    if (s.charAt(j) == closedParen && (j == jStart || s.charAt(j - 1) != closedParen))
+                        removeHelper(s.substring(0, j) + s.substring(j + 1, s.length()), output, i, j, openParen, closedParen);
+                return; // Stop here. The recursive calls handle the rest of the string.
+            }
+        }
+        // No more invalid closed parenthesis. Check opposite direction, or reverse back to original direction.
+        String reversed = new StringBuilder(s).reverse().toString();
+        if (openParen == '(')
+            removeHelper(reversed, output, 0, 0, ')','(');
+        else
+            output.add(reversed);
+    }
+    
+}
