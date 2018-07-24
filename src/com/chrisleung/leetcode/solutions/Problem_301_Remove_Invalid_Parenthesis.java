9fbc4edc7822fa5e3503c8d3b4a821f173ef201b
chrislzm <me@chrisleung.com>
2018-07-24 01:15:02 -0700

    doc: Added+updated comments

--- src/com/chrisleung/leetcode/solutions/Problem_301_Remove_Invalid_Parenthesis.java
+++ src/com/chrisleung/leetcode/solutions/Problem_301_Remove_Invalid_Parenthesis.java
@@ -19,11 +19,12 @@ public class Problem_301_Remove_Invalid_Parenthesis {
             if (numClosedParen > numOpenParen) { // We have an extra closed paren we need to remove
                 for (int j = jStart; j <= i; j++) // Try removing one at each position, skipping duplicates
                     if (s.charAt(j) == closedParen && (j == jStart || s.charAt(j - 1) != closedParen))
+                        // Recursion: iStart = i since we now have valid # closed parenthesis thru i. jStart = j prevents duplicates
                         removeHelper(s.substring(0, j) + s.substring(j + 1, s.length()), output, i, j, openParen, closedParen);
                 return; // Stop here. The recursive calls handle the rest of the string.
             }
         }
-        // No more invalid closed parenthesis. Check opposite direction, or reverse back to original direction.
+        // No invalid closed parenthesis detected. Now check opposite direction, or reverse back to original direction.
         String reversed = new StringBuilder(s).reverse().toString();
         if (openParen == '(')
             removeHelper(reversed, output, 0, 0, ')','(');
