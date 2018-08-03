9c163c155e40cdbf3aa8d2ca52f39b20ca23282e
chrislzm <me@chrisleung.com>
2018-08-02 21:31:25 -0700

    refactor: Cleaned up code

--- src/com/chrisleung/other/solutions/ChopString.java
+++ src/com/chrisleung/other/solutions/ChopString.java
@@ -6,52 +6,48 @@ package com.chrisleung.other.solutions;
  *
  */
 public class ChopString {
-    
+
     public static String getWords(String s, int k) throws IllegalArgumentException {
         if(s == null || s.length() == 0 || k <= 0) throw new IllegalArgumentException();
-        
+
         StringBuilder sb = new StringBuilder();
-        while(true) {
-          if(s.length() <= k) {
-            sb.append(s);
-            break;
-          }
-          int lastIndex = s.lastIndexOf(' ',k);
-          if(lastIndex > 0) {
+        while(s.length() > k) {
+            int lastIndex = s.lastIndexOf(' ',k);
+            if(lastIndex < 0) {
+                throw new IllegalArgumentException();
+            }
             sb.append(s.substring(0,lastIndex)).append('\n');
             s = s.substring(lastIndex+1);
-          } else {
-            throw new IllegalArgumentException();
-          }
         }
+        sb.append(s);
         return sb.toString();
     }
-    
+
     public static void main(String[] args) {
-      
-      System.out.println("Test case 1");
-      System.out.println(getWords("foo bar baz",3));
-      System.out.println("Test case 2");
-      System.out.println(getWords("foo bar baz",6));
-      System.out.println("Test case 3");
-      System.out.println(getWords("foo bar baz",7));
-      System.out.println("Test case 4");
-      System.out.println(getWords("foo bar baz",11));
-      System.out.println("Test case 5");
-      System.out.println(getWords("foo",3));
-      System.out.println("Test case 6");
-      System.out.println(getWords("foo",1));
-      System.out.println("Test case 7");
-      System.out.println(getWords("",5));
-      System.out.println("Test case 8");
-      System.out.println(getWords("foo",0));
-      
-      /*
+
+        System.out.println("Test case 1");
+        System.out.println(getWords("foo bar baz",3));
+        System.out.println("Test case 2");
+        System.out.println(getWords("foo bar baz",6));
+        System.out.println("Test case 3");
+        System.out.println(getWords("foo bar baz",7));
+        System.out.println("Test case 4");
+        System.out.println(getWords("foo bar baz",11));
+        System.out.println("Test case 5");
+        System.out.println(getWords("foo",3));
+        System.out.println("Test case 6");
+        System.out.println(getWords("foo",1));
+        System.out.println("Test case 7");
+        System.out.println(getWords("",5));
+        System.out.println("Test case 8");
+        System.out.println(getWords("foo",0));
+
+        /*
       String s = "foo bar baz";
       System.out.println(s.lastIndexOf(' ', 3));
       System.out.println(s.lastIndexOf(' ', 5));
       System.out.println(s.lastIndexOf(' ', 7));
-      */
+         */
     }
 
 }
