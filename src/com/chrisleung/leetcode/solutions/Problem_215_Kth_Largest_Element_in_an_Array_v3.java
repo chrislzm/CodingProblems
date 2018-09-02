7ae3549a2286a8cf95b8e06d1c0edffb04ddd1a1
chrislzm <me@chrisleung.com>
2018-09-02 16:25:23 -0700

    Initial commit: Problem 215 Kth Largest Element in an Array O(n) average running time algorithm

---
+++ src/com/chrisleung/leetcode/solutions/Problem_215_Kth_Largest_Element_in_an_Array_v3.java
@@ -0,0 +1,68 @@
+package com.chrisleung.leetcode.solutions;
+
+import java.util.Random;
+
+/**
+ * O(n) average case solution using partition method (of quicksort) and shuffle to randomize elements
+ * @author Chris Leung
+ *
+ */
+public class Problem_215_Kth_Largest_Element_in_an_Array_v3 {
+
+    public int findKthLargest(int[] nums, int k) {
+        shuffle(nums);
+        int loIndex = 0;
+        int hiIndex = nums.length-1;
+        int targetIndex = nums.length-k;
+        while(true) {
+            int partitionIndex = partition(nums,loIndex,hiIndex);
+            if(partitionIndex < targetIndex) {
+                loIndex = partitionIndex+1;
+            } else if(partitionIndex > targetIndex) {
+                hiIndex = partitionIndex-1;
+            } else { // partitionIndex == targetIndex
+                break;
+            }
+        }
+        return nums[targetIndex];
+    }
+    
+    /** 
+     * Quicksort partition method
+     * Handles two edge cases:
+     * 1) loIndex == hiIndex
+     * 2) loIndex = hiIndex-1, and pivotValue < hiIndex (in this case, hiIndex will be decremented past loIndex to the pivotIndex) 
+     */
+    private int partition(int[] nums, int loIndex, int hiIndex) {
+        int pivotValue = nums[loIndex];
+        int pivotIndex = loIndex;
+        loIndex++;
+        while(loIndex <= hiIndex) { 
+            if(nums[loIndex] < pivotValue) {
+                loIndex++;  
+            } else if(nums[hiIndex] >= pivotValue) {
+                hiIndex--;
+            } else {
+                swap(nums,loIndex,hiIndex);
+            }                
+        }
+        // All elements to the right of hiIndex are now greater than pivotValue
+        swap(nums,pivotIndex,hiIndex); 
+        return hiIndex;
+    }
+    
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    
+    private void shuffle(int[] arr) {
+        Random r = new Random();
+        int range = arr.length;
+        for(int i=0; i<range; i++) {
+            int swapIndex = r.nextInt(range);
+            swap(arr,i,swapIndex);
+        }
+    }
+}
